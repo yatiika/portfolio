9a2477a6264e3d025ec8861e9626a0c8bc73dbaa
Mamta Sharma <mamtahere.justmailme02@gmail.com>
2026-05-01 01:00:44 +0530

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,24 +1,20 @@
-import tailwindcss from '@tailwindcss/vite';
-import react from '@vitejs/plugin-react';
-import path from 'path';
-import {defineConfig, loadEnv} from 'vite';
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import tailwindcss from "@tailwindcss/vite";
+import path, { dirname } from "path";
+import { fileURLToPath } from "url";
 
-export default defineConfig(({mode}) => {
-  const env = loadEnv(mode, '.', '');
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
+export default defineConfig(({ mode }) => {
   return {
+    base: "/my_portfolio/",
     plugins: [react(), tailwindcss()],
-    define: {
-      'process.env.GEMINI_API_KEY': JSON.stringify(env.GEMINI_API_KEY),
-    },
     resolve: {
       alias: {
-        '@': path.resolve(__dirname, '.'),
+        "@": path.resolve(__dirname, "./src"),
       },
-    },
-    server: {
-      // HMR is disabled in AI Studio via DISABLE_HMR env var.
-      // Do not modifyâfile watching is disabled to prevent flickering during agent edits.
-      hmr: process.env.DISABLE_HMR !== 'true',
     },
   };
 });
